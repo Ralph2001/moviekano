b7bd1a03905a0e0b91e88f2aa4a864694937f6aa
Ralph2001 <villanuevaralph2001@gmail.com>
2025-04-21 11:31:34 +0800

    updated route

--- app/api/tv/[id]/season/[season]/route.ts
+++ app/api/tv/[id]/season/[season]/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import { tvApi } from "@/services/tmdbClient";
+import type { NextRequest } from "next/server";
 
 export async function GET(
-    _req: Request,
-    { params }: { params: { id: string; season: string } }
+    req: NextRequest,
+    context: { params: { id: string; season: string } }
 ) {
-    const tvId = Number(params.id);
-    const seasonNum = Number(params.season);
+    const tvId = Number(context.params.id);
+    const seasonNum = Number(context.params.season);
     try {
         const data = await tvApi.getTvSeasonDetails(tvId, seasonNum);
         return NextResponse.json(data);
